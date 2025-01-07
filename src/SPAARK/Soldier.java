a5125a542b76717eb62067f03ea8f41279606f74
Suvanth Erranki <76885647+Suvanth-Erranki@users.noreply.github.com>
2025-01-07 10:37:28 -0500

    BUH NO SOLDIER

--- src/SPAARK/Soldier.java
+++ src/SPAARK/Soldier.java
@@ -9,24 +9,27 @@ public class Soldier {
     public static MapLocation currLoc;
     
     public static void run() throws Exception {
-       for (int i = 0; i < 3; i++) {
-                if (rc.canMarkTowerPattern(Tower.moneyLevels[i], Motion.currLoc)) {
-                    rc.markTowerPattern(Tower.moneyLevels[i], Motion.currLoc);
-                    break;
-                }
+        for (int i = 0; i < 3; i++) {
+            if (rc.canMarkTowerPattern(Tower.moneyLevels[i], Motion.currLoc)) {
+                System.out.println(rc.getID() + " marked money tower pattern");
+                rc.markTowerPattern(Tower.moneyLevels[i], Motion.currLoc);
+                break;
             }
-        
-       for (int i = 0; i < 3; i++) {
+        }
+            
+        for (int i = 0; i < 3; i++) {
             if (rc.canMarkTowerPattern(Tower.paintLevels[i], Motion.currLoc)) {
+                System.out.println(rc.getID() + " marked paint tower pattern");
                 rc.markTowerPattern(Tower.paintLevels[i], Motion.currLoc);
                 break;
             }
         }
         
-        if (rc.canCompleteResourcePattern(Motion.currLoc)) {
+        if (rc.canCompleteResourcePattern(Motion.currLoc)) {        
+            System.out.println(rc.getID() + " completed tower pattern");
             rc.completeResourcePattern(Motion.currLoc);
         }
-        Motion.spreadRandomly();
+        Motion.moveRandomly();
         Clock.yield();
     }
 }
