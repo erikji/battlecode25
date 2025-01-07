259104757e35ad933d6577c31039c4c4c4affe16
Suvanth Erranki <76885647+Suvanth-Erranki@users.noreply.github.com>
2025-01-06 23:29:47 -0500

    tower spawning but borken will fix tmrw

---
+++ java/src/SPAWNTWR/PaintTower.java
@@ -0,0 +1,10 @@
+package SPAWNTWR;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class PaintTower {
+    public static void run(RobotController rc, Random rng) throws Exception {
+        
+    }
+}
---
+++ java/src/SPAWNTWR/Soldier.java
@@ -0,0 +1,10 @@
+package SPAWNTWR;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class Soldier {
+    public static void run(RobotController rc, Random rng) throws Exception {
+        Motion.currLoc = rc.getLocation();
+    }
+}
