d785bb1b7e231dd5caabbb85c78c9e093a07be2d
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-01-06 20:53:19 -0500

    i fixed ur code

--- java/src/SPAARK/RobotPlayer.java
+++ java/src/SPAARK/RobotPlayer.java
@@ -5,32 +5,29 @@ import java.util.*;
 
 public class RobotPlayer {
     protected static Random rng;
-    public static void run(RobotController rc) {
+
+    public static void run(RobotController rc) throws Exception {
         try {
-            rng = new Random(rc.getID()+2025);
+            rng = new Random(rc.getID() + 2025);
             Motion.rc = rc;
             Motion.rng = rng;
             Micro.rc = rc;
             Micro.rng = rng;
             switch (rc.getType()) {
                 case MOPPER:
-                    Robot.run(rc,rng);
-                    break;
                 case SOLDIER:
-                    Robot.run(rc,rng);
-                    break;
                 case SPLASHER:
-                    Robot.run(rc,rng);
+                    Robot.run(rc, rng);
                     break;
                 default:
-                    Tower.run(rc,rng);
+                    Tower.run(rc, rng);
                     break;
             }
         } catch (GameActionException e) {
-            System.out.println("GameActionException");
+            System.out.println("Unexpected GameActionException");
             e.printStackTrace();
         } catch (Exception e) {
-            System.out.println("Exception");
+            System.out.println("Unexpected Exception");
             e.printStackTrace();
         }
     }
--- java/src/SPAARK/Tower.java
+++ java/src/SPAARK/Tower.java
@@ -1,40 +1,42 @@
 package SPAARK;
 
 import battlecode.common.*;
+import java.util.*;
 
 public class Tower {
-    public static void run(RobotController rc) {
+    public static void run(RobotController rc, Random rng) throws Exception {
         while (true) {
             switch (rc.getType()) {
                 case LEVEL_ONE_DEFENSE_TOWER:
-                    DefenseTower.run(rc,1);
+                    DefenseTower.run(rc, rng, 1);
                     break;
                 case LEVEL_TWO_DEFENSE_TOWER:
-                    DefenseTower.run(rc,2);
+                    DefenseTower.run(rc, rng, 2);
                     break;
                 case LEVEL_THREE_DEFENSE_TOWER:
-                    DefenseTower.run(rc,3);
+                    DefenseTower.run(rc, rng, 3);
                     break;
                 case LEVEL_ONE_MONEY_TOWER:
-                    MoneyTower.run(rc,1);
+                    MoneyTower.run(rc, rng, 1);
                     break;
                 case LEVEL_TWO_MONEY_TOWER:
-                    MoneyTower.run(rc,2);
+                    MoneyTower.run(rc, rng, 2);
                     break;
                 case LEVEL_THREE_MONEY_TOWER:
-                    MoneyTower.run(rc,3);
+                    MoneyTower.run(rc, rng, 3);
                     break;
                 case LEVEL_ONE_PAINT_TOWER:
-                    PaintTower.run(rc,1);
+                    PaintTower.run(rc, rng, 1);
                     break;
                 case LEVEL_TWO_PAINT_TOWER:
-                    PaintTower.run(rc,2);
+                    PaintTower.run(rc, rng, 2);
                     break;
                 case LEVEL_THREE_PAINT_TOWER:
-                    PaintTower.run(rc,3);
+                    PaintTower.run(rc, rng, 3);
                     break;
                 default:
-                    throw Exception("buh");
+                    throw new Exception("Challenge Complete! How Did We Get Here?");
+            }
         }
     }
 }
