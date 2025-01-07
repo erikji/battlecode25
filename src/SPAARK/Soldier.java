d5a452accae195a7b3d9164918227e3dfae7fd9a
Suvanth Erranki <76885647+Suvanth-Erranki@users.noreply.github.com>
2025-01-07 11:04:32 -0500

    buh

--- src/SPAARK/Soldier.java
+++ src/SPAARK/Soldier.java
@@ -9,27 +9,6 @@ public class Soldier {
     public static MapLocation currLoc;
     
     public static void run() throws Exception {
-        for (int i = 0; i < 3; i++) {
-            if (rc.canMarkTowerPattern(Tower.moneyLevels[i], Motion.currLoc)) {
-                System.out.println(rc.getID() + " marked money tower pattern");
-                rc.markTowerPattern(Tower.moneyLevels[i], Motion.currLoc);
-                break;
-            }
-        }
-            
-        for (int i = 0; i < 3; i++) {
-            if (rc.canMarkTowerPattern(Tower.paintLevels[i], Motion.currLoc)) {
-                System.out.println(rc.getID() + " marked paint tower pattern");
-                rc.markTowerPattern(Tower.paintLevels[i], Motion.currLoc);
-                break;
-            }
-        }
-        
-        if (rc.canCompleteResourcePattern(Motion.currLoc)) {        
-            System.out.println(rc.getID() + " completed tower pattern");
-            rc.completeResourcePattern(Motion.currLoc);
-        }
-        Motion.moveRandomly();
-        Clock.yield();
+
     }
 }
--- src/SPAARK/Splasher.java
+++ src/SPAARK/Splasher.java
@@ -10,5 +10,28 @@ public class Splasher {
     
     public static void run() throws Exception {
         Motion.currLoc = currLoc = rc.getLocation();
+
+        if (rc.senseMapInfo(currLoc).getMark().equals(PaintType.EMPTY)) {
+            System.out.println("marked");
+            if (rc.canAttack(currLoc)) {
+                System.out.println("splasher adding paint");
+                rc.attack(currLoc);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (rc.canMarkTowerPattern(Tower.paintLevels[i], Motion.currLoc)) {
+                System.out.println(rc.getID() + " marked paint tower pattern");
+                rc.markTowerPattern(Tower.paintLevels[i], Motion.currLoc);
+                break;
+            }
+        }
+        
+        if (rc.canCompleteResourcePattern(Motion.currLoc)) {        
+            System.out.println(rc.getID() + " completed tower pattern");
+            rc.completeResourcePattern(Motion.currLoc);
+        }
+        Motion.moveRandomly();
+        Clock.yield();
     }
 }
