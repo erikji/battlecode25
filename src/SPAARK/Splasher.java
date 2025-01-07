664cf006869e986f5e177b3d3cd444ea1c26dffa
Suvanth Erranki <76885647+Suvanth-Erranki@users.noreply.github.com>
2025-01-07 16:26:11 -0500

    idk what im doing

--- src/SPAARK/Splasher.java
+++ src/SPAARK/Splasher.java
@@ -11,11 +11,10 @@ public class Splasher {
     public static boolean[][][] towerPatterns;
 
     public static void run() throws Exception {
-        Motion.currLoc = rc.getLocation();
 
         // Find nearby ruin
         MapLocation target = null;
-        while (target == null) {
+        if (target == null) {
             for (MapLocation m : rc.senseNearbyRuins(-1)) {
                 if (!Tower.towerLocs.toString().contains(m.x + "" + m.y + "|")) {
                     Tower.towerLocs.append(m.x + "" + m.y + "|");
@@ -26,48 +25,48 @@ public class Splasher {
             Motion.spreadRandomly();
             Motion.updateInfo();
         }
-
-        // System.out.println("Found ruin");
-        while (Motion.currLoc.distanceSquaredTo(target) > 25) {
-            Motion.bugnavTowards(target);
-            Motion.updateInfo();
-        }
-
-        // System.out.println("in range of ruin");
-        
-        while (true) {
-            Motion.bugnavAround(target, 1, 25);
-            Motion.updateInfo();
-
-            if (rng.nextInt(100) > 50) {
-                for (int i = 0; i < 3; i++) {
-                    if (rc.canMarkTowerPattern(Tower.paintLevels[i], target)) {
-                        System.out.println(rc.getID() + " marked paint tower pattern");
-                        rc.markTowerPattern(Tower.paintLevels[i], target);
-                        break;
-                    }
-                }
+         
+        if (target != null) {
+            if (rc.getLocation().distanceSquaredTo(target) > 25) {
+                Motion.bugnavTowards(target);
+                Motion.updateInfo();
             }
             else {
-                for (int i = 0; i < 3; i++) {
-                    if (rc.canMarkTowerPattern(Tower.moneyLevels[i], target)) {
-                        System.out.println(rc.getID() + " marked money tower pattern");
-                        rc.markTowerPattern(Tower.moneyLevels[i], target);
-                        break;
+                System.out.println("in range of ruin with dist: " + rc.getLocation().distanceSquaredTo(target));
+        
+                Motion.bugnavAround(target, 1, 25);
+                Motion.updateInfo();
+
+                if (!rc.senseMapInfo(rc.getLocation()).getMark().equals(PaintType.EMPTY)) {
+                    if (rc.canAttack(rc.getLocation())) {
+                        System.out.println("painting");
+                        rc.attack(rc.getLocation());
                     }
                 }
-            }
-            if (!rc.senseMapInfo(Motion.currLoc).getMark().equals(PaintType.EMPTY)) {
-                if (rc.canAttack(Motion.currLoc)) {
-                    System.out.println("painting");
-                    rc.attack(Motion.currLoc);
-                }
-            }
 
-            if (rc.canCompleteResourcePattern(target)) {
-                System.out.println(rc.getID() + " completed tower pattern");
-                rc.completeResourcePattern(target);
-                break;
+                if (rng.nextInt(100) > 50) {
+                    for (int i = 0; i < 3; i++) {
+                        if (rc.canMarkTowerPattern(Tower.paintLevels[i], target)) {
+                            System.out.println(rc.getID() + " marked paint tower pattern");
+                            rc.markTowerPattern(Tower.paintLevels[i], target);
+                            break;
+                        }
+                    }
+                }
+                else {
+                    for (int i = 0; i < 3; i++) {
+                        if (rc.canMarkTowerPattern(Tower.moneyLevels[i], target)) {
+                            System.out.println(rc.getID() + " marked money tower pattern");
+                            rc.markTowerPattern(Tower.moneyLevels[i], target);
+                            break;
+                        }
+                    }
+                }
+
+                if (rc.canCompleteResourcePattern(target)) {
+                    System.out.println(rc.getID() + " completed tower pattern");
+                    rc.completeResourcePattern(target);
+                }
             }
         }
     }
