8fd9eab2bf893c61192f7093110c60a0af7c7c90
SampleProvider <68207888+SampleProvider@users.noreply.github.com>
2025-01-08 21:23:45 -0500

    Update Robot.java

--- src/SPAARK/Robot.java
+++ src/SPAARK/Robot.java
@@ -53,7 +53,7 @@ public class Robot {
                 boolean bestPaint = false;
                 boolean bestCritical = false;
                 String tried = triedRetreatTowers.toString();
-                for (int i = 0; i < 50; i++) {
+                for (int i = 49; --i >= 0; ) {
                     if (POI.towers[i] == -1) {
                         break;
                     }
