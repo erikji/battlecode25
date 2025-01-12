77989004e7bef09b495b663559493ddaaf7f2c8e
SampleProvider <68207888+SampleProvider@users.noreply.github.com>
2025-01-12 18:53:36 -0500

    butterspinning

---
+++ src/betterspawning/DefenseTower.java
@@ -0,0 +1,7 @@
+package betterspawning;
+
+public class DefenseTower {
+
+    public static void run() throws Exception {
+    }
+}
---
+++ src/betterspawning/PaintTower.java
@@ -0,0 +1,7 @@
+package betterspawning;
+
+public class PaintTower {
+
+    public static void run() throws Exception {
+    }
+}
