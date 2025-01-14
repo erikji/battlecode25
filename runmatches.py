4c55c3e2c5a65d02e0419252af1c6a5314c0eaa1
SampleProvider <68207888+SampleProvider@users.noreply.github.com>
2025-01-14 15:21:55 -0500

    Update runmatches.py

--- runmatches.py
+++ runmatches.py
@@ -14,7 +14,7 @@ emojiMap = {
 }
 errors = []
 
-currentBot = 'TSPAARKJAN09' #bot to test
+currentBot = 'betterdisintegrating' #bot to test
 #SPAARK IS THE BASELINE
 
 # bots = [chr(ord('a')+i) for i in range(1)]
