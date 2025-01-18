ac6b0bae1b4c8c4702379bcf81ae819b6f6d3e39
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-01-18 10:33:09 -0500

    fix TSPAARK crashes

--- src/TSPAARKJAN16/Soldier.java
+++ src/TSPAARKJAN16/Soldier.java
@@ -425,7 +425,7 @@ public class Soldier {
                     // can't paint enemy paint
                     if (!exists.isEnemy() && (paint ? PaintType.ALLY_SECONDARY : PaintType.ALLY_PRIMARY) != exists) {
                         G.rc.attack(loc, paint);
-                        paintLocation = loc;
+                        // paintLocation = loc;
                         break;
                     }
                 }
@@ -467,7 +467,7 @@ public class Soldier {
                     // can't paint enemy paint
                     if (!exists.isEnemy() && (paint ? PaintType.ALLY_SECONDARY : PaintType.ALLY_PRIMARY) != exists) {
                         G.rc.attack(loc, paint);
-                        paintLocation = loc;
+                        // paintLocation = loc;
                         break;
                     }
                 }
