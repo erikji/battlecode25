e5117d02c25b3372bbf8ed4a0bc63f0262b046d9
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-01-20 22:07:40 -0500

    create TSPAARKSP2

---
+++ src/TSPAARKSP2/MoneyTower.java
@@ -0,0 +1,7 @@
+package TSPAARKSP2;
+
+public class MoneyTower {
+
+    public static void run() throws Exception {
+    }
+}
---
+++ src/TSPAARKSP2/PaintTower.java
@@ -0,0 +1,7 @@
+package TSPAARKSP2;
+
+public class PaintTower {
+
+    public static void run() throws Exception {
+    }
+}
