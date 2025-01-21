98ae3642f66e5a1506de332108d7dabed8bbcb62
erikji <temple.monkey.god@gmail.com>
2025-01-21 12:02:22 -0500

    a

--- src/SPAARK/POI.java
+++ src/SPAARK/POI.java
@@ -599,7 +599,7 @@ public class POI {
             }
         }
         for (Message m : messages) {
-            if (Clock.getBytecodesLeft() < 2000)
+            if (Clock.getBytecodesLeft() < 10000)
                 break;
             read16BitMessage(m.getSenderID(), m.getBytes() & 0b1111111111111111);
             if ((m.getBytes() >> 16) != 0) {
